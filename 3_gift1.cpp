be4c30ee2c07f42a5dc4f3b81b87b35ed7514c23
Conrad Kuzmick <33187769+ckuzmick@users.noreply.github.com>
2023-11-29 16:29:45 +0000

    stuff

--- 3_gift1.cpp
+++ 3_gift1.cpp
@@ -3,12 +3,16 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <sstream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
     string line;
     vector<string> array;
+    map<string, int> peopleDict;
 
     ifstream inputFile("./gift1.in");
 
@@ -24,8 +28,40 @@ int main() {
     while (i < numPeople) {
         string person;
         getline (inputFile, person);
-        cout << person << endl;
+        // cout << person << endl;
+        peopleDict.insert({person, 0});
         i++;
+    }
+
+    // while (true) {
+        string giftGiver;
+        getline (inputFile, giftGiver);
+
+        cout << giftGiver;
+
+        string lineUnprocesssed;
+        getline (inputFile, lineUnprocesssed);
+
+        string token;
+ 
+        stringstream ss(lineUnprocesssed);
+
+        vector<string> num;
+
+        while (getline(ss, token, ' ')) {
+            num.push_back(token);
+        }
+        
+        int amount = stoi(num[0]);
+        int recievers = stoi(num[1]);
+
+        int APP = ceil(amount / recievers);
+
+        cout << amount << giftGiver << recievers << endl << APP << endl;
+    // }
+
+    for (auto itr = peopleDict.begin(); itr != peopleDict.end(); ++itr) {
+      cout << itr->first << " " << itr->second << endl;
     }
 
     while (getline (inputFile, line)) {
