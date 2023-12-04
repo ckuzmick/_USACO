a84e81d5d7660c87919230fdd1448440cb78445a
Conrad Kuzmick <33187769+ckuzmick@users.noreply.github.com>
2023-12-04 09:20:23 -0500

    stuff

--- 8_namenum.cpp
+++ 8_namenum.cpp
@@ -3,12 +3,12 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <map>
 
 using namespace std;
 
 int main() {
     // get input from file;
-
     ifstream inputFile;
     inputFile.open("namenum.in");
     string stringNum;
@@ -17,6 +17,16 @@ int main() {
     inputFile.close();
 
     // define variables;
+    vector<string> names;
+    map<int, char[3]> numbers;
+        numbers[2] = {'A', 'B', 'C'};
+        numbers[3] = {'D', 'E', 'F'};
+        numbers[4] = {'G', 'H', 'I'};
+        numbers[5] = {'J', 'K', 'L'};
+        numbers[6] = {'M', 'N', 'O'};
+        numbers[7] = {'P', 'R', 'S'};
+        numbers[8] = {'T', 'U', 'V'};
+        numbers[9] = {'W', 'X', 'Y'};
 
     // fill vector will all possible names;
 
