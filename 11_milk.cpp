976f7aaa409ee4147b78896d35b182431899db35
ckuzmick <conrad.kuzmick@icloud.com>
2023-12-11 22:16:25 -0500

    change

---
+++ 11_milk.cpp
@@ -0,0 +1,18 @@
+/*
+ID: 27ckuzm1
+TASK: dualpal
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
